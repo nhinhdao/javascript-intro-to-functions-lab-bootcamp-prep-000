18e4b930d6dc0e0416bcc12dc63a5774758fcb6a
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-08 06:12:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string) {
 function logWhisper(string) {
    console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+   var newstring = string;
+   if (newstring.toLowerCase() === string){
+      return "I can't hear you!"
+   }
+   else if (newstring.toUpperCase() === string){
+      return "YES INDEED!";
+   }
+   else {
+      return "I love you, too.";
+   }
+}
