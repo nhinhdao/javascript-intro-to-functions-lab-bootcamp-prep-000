20004583b0d4d321875af7eb7db142ec6ae44d08
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-08 06:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string) {
    return string.toUpperCase()
 }
+
+function shout(string) {
+   return string.toLowerCase()
+}
+
+function logshout(string) {
+   console.log(string.toUpperCase());
+}
+
+function logshout(string) {
+   console.log(string.toLowerCase());
+}
